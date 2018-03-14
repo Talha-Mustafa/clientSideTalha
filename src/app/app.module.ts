e8739f954e5130321ce2a78154cbe603b72caefe
Talha-Mustafa <32811523+Talha-Mustafa@users.noreply.github.com>
2018-03-14 23:08:28 +0500

    firstDay
    client side by talhaMustafa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,60 @@
+
+//*****************************ANGULAR MODULES************************
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
+//********************************************************************
 
+//*****************************COMPONENTS*****************************
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { AddListingComponent } from './add-listing/add-listing.component';
+
+
+
+//*********************************************************************
+
+
+//*****************************NGX-BOOSTRAP****************************
+ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+ import { TooltipModule } from 'ngx-bootstrap/tooltip';
+ import { ModalModule } from 'ngx-bootstrap/modal';
+
+//*********************************************************************
+
+
+
+//************IMPORTING ROUTER FILE**********
+import { appRoutes } from '../routes';
+//*******************************************
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CategoriesComponent,
+    AddListingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
+//INCLUDING ROUTER
+    RouterModule.forRoot(appRoutes),
+//*****************
+
+
+
+//*******NGX-BOOSTRAP**********
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
+//******************************
   ],
+  exports: [BsDropdownModule, TooltipModule, ModalModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+ import { HomeComponent } from './app/home/home.component';
+ import { CategoriesComponent } from './app/categories/categories.component';
+ import { AddListingComponent } from './app/add-listing/add-listing.component';
+ import { AuthenticationGuardService } from './app/services/authentication-guard.service';
+ import { Routes } from '@angular/router';
+
+
+ export const appRoutes:  Routes =[];
